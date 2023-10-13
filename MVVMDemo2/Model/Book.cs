3583062c89bdf5bddb7b7cfdd7b4192720197c07
GuenKainto <khanhtho10122002@gmail.com>
2023-10-13 08:31:15 +0700

    Add project files.

---
+++ MVVMDemo2/Model/Book.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.RightsManagement;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.ComponentModel;
+
+namespace MVVMDemo2.Model
+{
+    public class BookModel { }
+    public class Book : INotifyPropertyChanged
+    {
+
+        public Book() { }
+        public Book(string id, string name, string dateRelease, string publisher)
+        {
+            Id = id;
+            Name = name;
+            DateRelease = dateRelease;
+            Publisher = publisher;
+        }
+
+        private string _id { get; set; }
+
+        public string Id
+        {
+            get { return _id; }
+            set
+            {
+                if (_id != value)
+                {
+                    _id = value;
+                    RaisePropertyChanged(nameof(Id));
+                }
+            }
+        }
+
+        private string _name { get; set; }
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    RaisePropertyChanged(nameof(Name));
+                }
+            }
+        }
+
+        private string _dataRelease { get; set; }
+        public string DateRelease
+        {
+            get => _dataRelease;
+            set
+            {
+                if (_dataRelease != value)
+                {
+                    _dataRelease = value;
+                    RaisePropertyChanged(nameof(DateRelease));
+                }
+            }
+        }
+
+        private string _publisher { get; set; }
+        public string Publisher
+        {
+            get => _publisher;
+            set
+            {
+                if (_publisher != value)
+                {
+                    _publisher = value;
+                    RaisePropertyChanged(nameof(Publisher));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+}
---
+++ MVVMDemo2/ViewModel/BookViewModel.cs
@@ -0,0 +1,32 @@
+﻿using MVVMDemo2.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMDemo2.ViewModel
+{
+    public class BookViewModel
+    {
+        public List<Book> ListBook
+        {
+            get; set;
+        }
+        public BookViewModel() 
+        {
+            AddTempBookIntoList();
+        }
+        public void AddTempBookIntoList()
+        {
+            List<Book> books = new List<Book>();
+            books.Add(new Book("B01","C# for beginner","10/11/2012","WibuCoder"));
+            books.Add(new Book("B02","C++ for beginner", "09/11/2014", "WibuCoder"));
+            books.Add(new Book("B03","Welcome to Hello World", "11/01/2010", "WibuCoder"));
+            books.Add(new Book("B04","Life", "17/02/2012", "WibuCoder"));
+
+            ListBook = books;
+        }
+    }
+}
